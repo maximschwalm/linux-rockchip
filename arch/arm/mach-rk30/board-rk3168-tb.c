c0eb187b1ca2726fe4cb2fb42efc62cefc3dee93
Maxim Schwalm <maxim.schwalm@gmail.com>
2021-03-23 23:18:14 +0100

    rk3168 tb: HYM8563 rtc: add customizations for Medion Lifetab S785X

--- arch/arm/mach-rk30/board-rk3168-tb.c
+++ arch/arm/mach-rk30/board-rk3168-tb.c
@@ -2474,7 +2474,7 @@ static struct i2c_board_info __initdata i2c1_info[] = {
 		.type                   = "rtc_hym8563",
 		.addr           = 0x51,
 		.flags                  = 0,
-		.irq            = RK30_PIN1_PA4,
+        .irq            = RK30_PIN0_PB5,
 	},
 #if defined (CONFIG_CW2015_BATTERY)
         {
